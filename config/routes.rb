14b027fad7661392fe14f0d5290b917ec5ec9233
Carlos Aguirre <carlostomasaguirre@yahoo.com>
2023-05-29 16:33:09 +0200

    tweaks to routers

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,13 @@ Rails.application.routes.draw do
   root to: "pages#home" # Home page
 
   get "/instruments", to: "instruments#index" # Browse offers
-  get "/instruments/:id", to: "instruments#show" # View instrument details
+
 
   get "/instruments/:instrument_id/bookings/new", to: "bookings#new" # Select instrument(s) - form for creating a booking
   post "/instruments/:instrument_id/bookings", to: "bookings#create" # Select instrument(s) - create a booking
 
   get "/instruments/new", to: "instruments#new" # Lend instrument - form for creating a new instrument
   post "/instruments", to: "instruments#create" # Lend instrument - create a new instrument
-
+  get "/instruments/:id", to: "instruments#show" # View instrument details
   get "/bookings", to: "bookings#index" # View all bookings
 end
