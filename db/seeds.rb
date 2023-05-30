114e7c15b6649205fb692f2beb3e9cab4ab103b7
Carlos Aguirre <carlostomasaguirre@yahoo.com>
2023-05-30 16:38:20 +0200

    seed part 1 done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-User.destroy_all
+puts "destroying all..."
+Booking.destroy_all
 Instrument.destroy_all
+User.destroy_all
 
-User.create!(email: "isalopez@gmail.com", encrypted_password: "123456")
-User.create!(email: "guggi.schneck@gmail.com", encrypted_password: "123456")
+puts "creating users"
+user1 = User.create!(email: "isalopez@gmail.com", password: "123456")
+user2 = User.create!(email: "guggi.schneck@gmail.com", password: "123456")
 
-Instrument.create!(category: "triangle", price: 8, serial: 00762)
-Instrument.create!(category: "trombone", price: 18, serial: 00882)
-Instrument.create!(category: "guitar", price: 22, serial: 00793)
+puts "creating instruments"
+Instrument.create!(category: "triangle", price: 8, serial: 762, user: user1)
+Instrument.create!(category: "trombone", price: 18, serial: 202, user: user2)
+Instrument.create!(category: "guitar", price: 22, serial: 403, user: user1)
