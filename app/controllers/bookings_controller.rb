f95584ed1387a18608c5e35ec0ca6260c9fcb275
Carlos Aguirre <carlostomasaguirre@yahoo.com>
2023-05-30 10:48:11 +0200

    bookingsflowdone

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,5 @@
 class BookingsController < ApplicationController
+  before_action :set_instrument, only: [:new, :create]
   def index
     @bookings = Booking.all
   end
@@ -8,9 +9,9 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @instrument = Instrument.find(params[:instrument_id])
     @booking = Booking.new(booking_params)
     @booking.instrument = @instrument
+    @booking.user = current_user
     if @booking.save
       redirect_to bookings_path(@booking)
     else
@@ -19,6 +20,10 @@ class BookingsController < ApplicationController
   end
 
   private
+
+  def set_instrument
+    @instrument = Instrument.find(params[:instrument_id])
+  end
 
   def booking_params
     params.require(:booking).permit(:booking_startdate, :booking_enddate, :user_id)
